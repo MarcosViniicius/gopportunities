edef6050a47db8520a577f93eec50e7c2f49c059
Marcos Vinicius <marcosvinicius.lucena27@gmail.com>
2026-01-08 10:28:52 -0300

    I separated routes into individual files.

--- handler/handler.go
+++ handler/handler.go
@@ -1,33 +1,3 @@
 package handler
 
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
-func ShowOpeningHandler(ctx *gin.Context){
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET opening",
-	})
-}
-
-func CreateOpeningHandler(ctx *gin.Context){
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "post opening",
-	})
-}
-func DeleteOpeningHandler(ctx *gin.Context){
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "delete opening",
-	})
-}
-func UpdateOpeningHandler(ctx *gin.Context){
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "put opening",
-	})
-}
-func ListOpeningsHandler(ctx *gin.Context){
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "list opening",
-	})
-}
+///
